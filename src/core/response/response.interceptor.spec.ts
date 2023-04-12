1d5535488c8860957d7b2e28fa49045769b4bb66
Alexander Zharkov <aleksandr.zharkov1@epam.com>
2023-04-12 14:27:57 +0600

    [unified-response-interface] Tests for ResponseInterceptor

---
+++ src/core/response/response.interceptor.spec.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import { ExecutionContext } from '@nestjs/common';
+import { ResponseInterceptor } from './response.interceptor';
+import { of, Observable } from 'rxjs';
+
+import { ResponseType } from './response.type';
+
+describe('ResponseInterceptor', () => {
+  let interceptor: ResponseInterceptor<string>;
+
+  beforeEach(() => {
+    interceptor = new ResponseInterceptor();
+  });
+
+  const getArguments = (statusCode: number, result: string) => {
+    const httpArgumentsHost = {
+      getResponse: () => ({
+        statusCode,
+      }),
+    };
+    const context = {
+      switchToHttp: () => httpArgumentsHost,
+    } as ExecutionContext;
+
+    const next = {
+      handle: (): Observable<string> => {
+        return of(result);
+      },
+    };
+
+    return { context, next };
+  };
+
+  describe('intercept', () => {
+    it('should return a response with success true for 2xx status codes', () => {
+      const { context, next } = getArguments(200, 'Hello world');
+      const observable: Observable<ResponseType<string>> = interceptor.intercept(context, next);
+
+      observable.subscribe((response: ResponseType<string>) => {
+        expect(response).to.deep.equal({
+          success: true,
+          data: 'Hello world',
+        });
+      });
+    });
+
+    it('should return a response with success false for 4/5xx status codes', () => {
+      const { context, next } = getArguments(404, 'Not Found');
+      const observable: Observable<ResponseType<string>> = interceptor.intercept(context, next);
+
+      observable.subscribe((response: ResponseType<string>) => {
+        expect(response).to.deep.equal({
+          success: false,
+          data: 'Not Found',
+        });
+      });
+    });
+  });
+});
