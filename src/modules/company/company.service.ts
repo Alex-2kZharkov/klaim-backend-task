27d6e01ba78631f335f1ff76d11ae3d514a744bf
Alexander Zharkov <aleksandr.zharkov1@epam.com>
2023-04-20 20:20:14 +0600

    [https-setup] Fix https

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -4,6 +4,6 @@ import { CompanyInfo } from './company.types';
 @Injectable()
 export class CompanyService {
   getInfo(): CompanyInfo {
-    return { info: 'Some information about the <b>company</b>.' };
+    return { info: 'Little story about the company' };
   }
 }
